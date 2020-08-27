fab4d2bfe9a8a5765567bffb75c5b6593f7cabb6
hmurataj <hmurataj@veicoliapp.com>
2020-08-27 10:33:05 +0200

    Fixing Register card bottom padding

--- lib/src/widgets/auth_card.dart
+++ lib/src/widgets/auth_card.dart
@@ -746,7 +746,7 @@ class _LoginCardState extends State<_LoginCard> with TickerProviderStateMixin {
             ),
           ),
           Container(
-            padding: Paddings.fromRBL(cardPadding),
+            padding: EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 2),
             width: cardWidth,
             child: Column(
               children: <Widget>[
